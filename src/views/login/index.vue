6a26abd8bdeba9f588e37a0d04cfe6ac25c720e3
Yuan Lee <yuanlee301415@126.com>
2026-04-07 09:29:05 +0800

    feat: 删除登录密码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,6 +5,7 @@ import { noSideSpace } from '@/utils/naiveUI.js'
 
 defineOptions({ name: 'LoginPage' })
 
+const NAME = import.meta.env.VITE_APP_TITLE
 const authStore = useAuthStore()
 const routeStore = useRouteStore()
 
@@ -16,7 +17,6 @@ const rules = {
 
 const formData = reactive({
   userName: '',
-  password: '',
 })
 
 const loading = ref(false)
@@ -36,16 +36,12 @@ async function handleSubmit() {
       <div class="min-w-600px">
         <n-flex vertical class="flex-center">
           <img src="/favicon.svg" width="48" alt="Favicon" />
-          <h1 class="font-bold">OpsClaw</h1>
+          <h1 class="font-bold">{{ NAME }}</h1>
         </n-flex>
 
         <n-form ref="formRef" :model="formData" :rules="rules" size="large" class="mt-10" @keyup.enter="handleSubmit">
           <n-form-item label="用户名" path="userName">
-            <n-input v-model:value="formData.userName" :allow-input="noSideSpace" placeholder="请输入用户名" maxlength="10" />
-          </n-form-item>
-
-          <n-form-item label="密码" path="password">
-            <n-input v-model:value="formData.password" :allow-input="noSideSpace" placeholder="密码随便填" type="password" maxlength="20" />
+            <n-input v-model:value="formData.userName" :allow-input="noSideSpace" placeholder="随便填" maxlength="10" />
           </n-form-item>
         </n-form>
 
